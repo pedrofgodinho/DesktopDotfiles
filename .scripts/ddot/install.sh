b28c7b67d615a8633b412ee7494589924ff83e86
pedrofgodinho <pedromfgodinho@gmail.com>
2021-06-21 03:51:50 +0100

    Updated install, updated wallpaper location

--- .scripts/ddot/install.sh
+++ .scripts/ddot/install.sh
@@ -6,6 +6,9 @@ sudo sed -i '/^\[Seat:\*]/a user-session=i3wm' /etc/lightdm/lightdm.conf
 sudo sed -i '/^\[Seat:\*]/a greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
 sudo systemctl enable lightdm.service
 
+sudo sh -c 'echo "[Greeter]" > /etc/lightdm/slick-greeter.conf'
+sudo sh -c 'echo "background=/usr/share/pixmaps/wallpaper.png" >> /etc/lightdm/slick-greeter.conf'
+
 mkdir --parents $HOME/.local/share/fonts
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O "$HOME/.local/share/fonts/MesloLGS NF Regular.ttf"
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O "$HOME/.local/share/fonts/MesloLGS NF Bold.ttf"
@@ -13,4 +16,10 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20I
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O "$HOME/.local/share/fonts/MesloLGS NF Bold Italic.ttf"
 fc-cache -f -v
 
-wget -o ~/.wallpaper/wallpaper.png https://i.redd.it/pm1yv0e8sf061.png
+wget -o /usr/share/pixmaps/wallpaper.png https://i.redd.it/pm2yv0e8sf061.png
+
+alias ddot='/usr/bin/git --git-dir=$HOME/.ddot --work-tree=$HOME'
+git clone --bare https://github.com/pedrofgodinho/DesktopDotfiles.git $HOME/.ddot
+ddot stash
+ddot checkout
+ddot config --local status.showUntrackedFiles no
